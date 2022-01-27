a05909424a1c4a4b04fac7a1f89de5c0b93b3ca5
Thisguy217 <87245214+Thisguy217@users.noreply.github.com>
2022-01-27 02:42:59 -0700

    Create ASCII.c

---
+++ C-Projects/ASCII.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   printf("   *");
+   printf("  ***");
+   printf(" *****");
+   printf("*******");
+   printf("  ***");
+   printf("\n\n");
+   printf("/\\   /\\");
+   printf("  o o");
+   printf(" =   =");
+   printf("  ---");
+
+   return 0;
+}
